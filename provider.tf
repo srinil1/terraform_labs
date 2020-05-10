9d4e9b7cf5bfdc9753ae3398d7d22d257c67caab
Srinil4 <connectsrinivaslanda@gmail.com>
2020-05-10 17:04:37 +0530

    commit terraform file

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region  = "ap-south-1"
+  access_key= "XXXXXXXX557J37AWK"
+  secret_key="XXXXXXXXXXXXn3HpxSXg8OtWUIEQRJ"
+}
---
+++ rds.tf
@@ -0,0 +1,13 @@
+resource "aws_db_instance" "default" {
+ allocated_storage = 5
+ storage_type      = "gp2"
+ engine 	   = "mysql"
+ engine_version    = "5.7"
+ instance_class    = "db.t2.micro"
+ name		   = "mydb"
+ username	   = "foo"
+ password          = "mysecretpassword505"
+ parameter_group_name = "default.mysql5.6"
+ skip_final_snapshot = "true"
+
+}
